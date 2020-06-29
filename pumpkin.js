0f9da0fd14303cceccafb2bb89f6b3d36ff82865
chadalad <cnuckols7@gmail.com>
2020-06-29 18:58:05 -0400

    pumpkin

--- pumpkin.js
+++ pumpkin.js
@@ -3,5 +3,31 @@ const Sequelize = require('sequelize');
 
 //YOUR CODE GOES HERE
 
+const Pumpkin = db.define('pumpkin', {
+  name: {
+    type: Sequelize.STRING,
+    primaryKey: true
+  },
+  size: {
+    type: Sequelize.ENUM,
+    validate: {
+      isIn: {
+        args: ['small', 'medium', 'large']
+      }
+    }
+  },
+  evil: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false,
+  },
+  carved: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false,
+  },
+  candle: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false,
+  }
+});
 //--------------------
 module.exports = Pumpkin;
